58ac8dc464c03bb94579bffbd79d73372322e665
siddharth bharal <bharalsiddharth7@gmail.com>
2024-07-04 23:11:50 +0000

    unique session

---
+++ models/example/uniquesession.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='incremental',
+    unique_key='session_id'
+) }}
+
+with source_data as (
+    select * 
+    from {{ source('ga4_obfuscated_sample_ecommerce', 'events_20210131') }}
+),
+
+sessions as (
+    select
+        concat(user_pseudo_id, '-', cast(event_timestamp as string)) as session_id,
+        user_pseudo_id,
+        event_date,
+        event_timestamp,
+        event_name,
+        event_server_timestamp_offset,
+        user_first_touch_timestamp,
+        geo.country,
+        geo.region,
+        geo.city,
+        traffic_source.medium,
+        traffic_source.source,
+        traffic_source.name as campaign
+    from
+        source_data
+)
+
+select * from sessions
