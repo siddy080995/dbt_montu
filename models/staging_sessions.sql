9024dcb7cb87c56d842401cea1165183f17f9995
siddharth bharal <bharalsiddharth7@gmail.com>
2024-07-07 00:47:02 +0000

    take everything in source data

--- models/staging_sessions.sql
+++ models/staging_sessions.sql
@@ -1,45 +1,32 @@
-
+-- Configuration
 {{ config(
   materialized='table',
   cluster_by=['session_id']
 ) }}
 
 
-
+-- Get the relevant columns from source data
 with source_data as (
     select 
-        event_date,
-        event_timestamp,
-        event_previous_timestamp,
-        cast(event_name AS STRING) AS event_name,
-        user_pseudo_id,
-        user_first_touch_timestamp,
-        device.category as device_category,
-        geo.country,
-        geo.region,
-        geo.city,
-        traffic_source.medium,
-        traffic_source.source,
-        traffic_source.name,
-        concat(user_pseudo_id, cast(event_timestamp as string)) as session_id  -- unique key session_id by concatenating user_pseudo_id and event_timestamp
+    *
     from {{ source('ga4_obfuscated_sample_ecommerce', 'events_20210131') }}
 )
 
 select 
-    session_id,
     user_pseudo_id,
     event_date,
     event_timestamp,
     event_previous_timestamp,
-    event_name,
+    cast(event_name AS STRING) AS event_name,
     user_first_touch_timestamp,
-    device_category,
-    country,
-    region,
-    city,
-    medium,
-    source,
-    name
+    device.category as device_category,
+    geo.country,
+    geo.region,
+    geo.city,
+    traffic_source.medium,
+    traffic_source.source,
+    traffic_source.name,
+    concat(user_pseudo_id, event_timestamp) as session_id  -- unique key session_id by concatenating user_pseudo_id and event_timestamp
 from source_data
 
 
